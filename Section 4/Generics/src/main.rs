0965be82689e7befe4f92bb2d0409134ac5ef7ec
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-11-11 10:33:53 -0300

    Learning about Traits

--- Section 4/Generics/src/main.rs
+++ Section 4/Generics/src/main.rs
@@ -4,9 +4,41 @@ struct Point<T, U> {
     y: U,
 }
 
+trait Overview {
+    fn overview(&self) -> String {
+        String::from("default implementation")
+    }
+}
+
+struct Course {
+    headline: String,
+    author: String
+}
+
+impl Overview for Course {
+    // default implementation
+}
+
+struct AnotherCourse {
+    headline: String,
+    author: String
+}
+
+impl Overview for AnotherCourse {
+    fn overview(&self) -> String {
+        format!("{}\n{}",self.headline, self.author)
+    }
+}
+
 fn main() {
 
     let coord = Point{x: 5.0, y: 5.0};
     let coord2 = Point{x: "5", y: 5.0};
+
+    let couser1 = Course{headline: String::from("Headline"), author: String::from("Author")};
+    let couser2 = AnotherCourse{headline: String::from("Headline 2"), author: String::from("Author 2")};
+
+    println!("{}",couser1.overview());
+    println!("{}",couser2.overview());
 
 }
